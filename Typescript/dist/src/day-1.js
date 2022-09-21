b9bfaa40bcaeaf83a3dc6f40ebc4d63d14b8f5ba
hellnoid <xainhellnoid22@gmail.com>
2022-09-21 12:33:58 +0530

    1

---
+++ Typescript/dist/src/day-1.js
@@ -0,0 +1,14 @@
+"use strict";
+console.log("hey world");
+let a = 67;
+a = 89;
+let b = "wewewe";
+console.log("a ", a);
+console.log("b ", b);
+console.log("Testting tsc");
+let employee = {
+    name: "Rishu",
+    id: 25
+};
+console.log(employee);
+//# sourceMappingURL=day-1.js.map
---
+++ Typescript/src/day-1.ts
@@ -0,0 +1,17 @@
+console.log("hey world");
+
+// declaration of variable
+let a: number = 67;
+a=89;
+let b: string= "wewewe";
+// b="wao! really?"
+console.log("a ",a);
+console.log("b ",b);
+console.log("Testting tsc");
+// how to take an object
+
+let employee:{}={
+    name:"Rishu",
+    id:25
+}
+console.log(employee);
